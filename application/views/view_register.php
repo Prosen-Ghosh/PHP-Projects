babb2c64e817525b7f83bc354ce7ba93716a647a
Prosen-Ghosh <prosenghosh25@gmail.com>
2016-11-03 06:23:50 +0600

    3/11/16 Last Change

--- application/views/view_register.php
+++ application/views/view_register.php
@@ -1,1 +1,38 @@
-<div><h1>This Will Be the Register page.</h1></div>
+<div class="logo logoSize" ></div>
+<center>
+  <div class="register">
+    <form action="" method="post">
+      <table>
+        <tr>
+          <td class="title">Name: </td>
+          <td><input type="text" name="name" value="" placeholder="Enter Your Full Name"></td>
+          <td><p class="error"></p></td>
+        </tr>
+        <tr>
+          <td class="title">User Name: </td>
+          <td><input type="text" name="userName" value="" placeholder="Enter a Unique User Name"></td>
+          <td><p class="error"></p></td>
+        </tr>
+        <tr>
+          <td class="title">Email: </td>
+          <td><input type="text" name="email" value="" placeholder="Enter Email: example@something.com"></td>
+          <td><p class="error"></p></td>
+        </tr>
+        <tr>
+          <td class="title">Password: </td>
+          <td><input type="password" name="password" value="" placeholder="Enter Password"></td>
+          <td><p class="error"></p></td>
+        </tr>
+        <tr>
+          <td class="title">Confrim Password: </td>
+          <td><input type="password" name="confrimPassword" value="" placeholder="Re-Enter Password"></td>
+          <td><p class="error"></p></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><input type="submit" name="submit" value="Register"></td>
+        </tr>
+      </table>
+    </form>
+  </div>
+</center>
--- public/styles/style.cs
+++ public/styles/style.cs
@@ -53,7 +53,61 @@
   height: 580px;
 }
 .logoSize {
-  height: 200px;
-  width: 220px;
+  height: 130px;
+  width: 150px;
   background-repeat: no-repeat;
+  background-color: #f2f2f2;
+}
+body{
+  background-color: #f2f2f2;
+}
+p.error{
+  color:red;
+}
+.register {
+  margin-top: 60px;
+}
+input[type=text], select {
+    width: 300px;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+input[type=password], select {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+
+input[type=submit] {
+    width: 100%;
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+input[type=submit]:hover {
+    background-color: #45a049;
+}
+
+div.register {
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+}
+td.title {
+  font-style: italic;
+  font-family: arial;
+  font-weight: bold;
 }
