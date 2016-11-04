c3484a94de242d12426a533a7be049f2cd74f8c6
Prosen-Ghosh <prosenghosh25@gmail.com>
2016-11-04 06:32:07 +0600

    03/11/16 Last Update

--- application/controllers/userhome.php
+++ application/controllers/userhome.php
@@ -1,13 +1,14 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Userhome extends CI_Controller {
   public function index(){
-    $username = $this->session->userdata('username');
-    if(isset($username)){
+    if($this->session->userdata('username')){
       $data['title'] = 'Home';
       $this->load->view('view_header',$data);
       $this->load->view('view_userhome');
       $this->load->view('view_footer');
     }
-    else echo "Na Dekhte Parba na...";
+    else {
+      redirect('http://localhost/coder/login');
+    }
   }
 }
