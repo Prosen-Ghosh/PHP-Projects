c693050efae1e9ac7114ada3f3bb26e0e1ed3e41
Prosen-Ghosh <prosenghosh25@gmail.com>
2016-11-04 19:57:17 +0600

    Logout Done

--- application/views/view_userhome.php
+++ application/views/view_userhome.php
@@ -1,1 +1,18 @@
+<div class="header">
+<div class="logo logoSize"></div>
+<div class="position">
+	<nav>
+		<ul>
+			<li><a href="/coder/">Home</a></li>
+			<li><a href="#post">Posts</a></li>
+      <li style='float:right'>
+        <select name="userinfo" onchange="location = this.value">
+          <option value=''><a class="active" href=""><?php echo ucfirst($this->session->userdata('username'));?></a></option>
+          <option value='/coder/logout'>Logout</option>
+        </select>
+      </li>
+		</ul>
+	</nav>
+</div>
+</div>
 <h1>Welcome <?php echo $this->session->userdata('username');?></h1>
